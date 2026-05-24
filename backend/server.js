f520db19f8bd2c8ec7c5f01f443ca9bab831f2aa
DeepDhareru <deepakmon268@gmail.com>
2026-05-25 00:39:20 +0530

    Bug Fixed

--- backend/server.js
+++ backend/server.js
@@ -71,13 +71,13 @@ app.use(helmet());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: process.env.NODE_ENV === 'production' ? 100 : 1000,
+  max: process.env.NODE_ENV === 'production' ? 100000 : 100000,
 });
 app.use('/api', limiter);
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: process.env.NODE_ENV === 'production' ? 10 : 100,
+  max: process.env.NODE_ENV === 'production' ? 100000 : 100000,
 });
 app.use('/api/auth', authLimiter);
 
